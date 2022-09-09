0caf9f8893520468161a3a6616f1bab4682e9e11
Leandro Carvalho <lrcarvalho@gmail.com>
2022-09-09 08:37:12 -0300

    Update github_api.js

--- js/github_api.js
+++ js/github_api.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
     dataType: "json",
     success: function (data) {
       x = data.name;
-      //console.log("User/Repo: " + data.full_name + " Star: " + data.stargazers_count+ " Forks: "+data.forks_count + " Watchers: "+data.watchers_count);
+      console.log("User/Repo: " + data.full_name + " Star: " + data.stargazers_count+ " Forks: "+data.forks_count + " Watchers: "+data.watchers_count);
       $("div[repo='" + x + "']").find("span[class='star']").html("&nbsp;"+data.stargazers_count);
       $("div[repo='" + x + "']").find("span[class='fork']").html("&nbsp;"+data.forks_count);
       $("div[repo='" + x + "']").find("span[class='watchers']").html("&nbsp;"+data.watchers_count);
